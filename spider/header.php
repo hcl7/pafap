3ce6a65a95aed54f5ef1b0550173f40f6453aebe
Elvin Mucaj <emuco7@yahoo.com>
2020-05-26 00:06:22 +0200

    Delete header.php

--- spider/header.php
+++
@@ -1,5 +0,0 @@
-<body><table width="800" align="center">
-<tr><td>
-<p></p><p></p>
-<p align="center"><a href="http://localhost/spider/index.php"><img src="http://localhost/spider/LOGO.png" alt="Spider" width="346" height="119" border="0"></a></p>
-<p></p>  
